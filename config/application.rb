74b7be50cfa01f3ed2ae99d75d7ef3b20f3ff90a
antebarac <abarac@gmail.com>
2014-06-26 09:10:02 +0200

    Locales

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Hana
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :hr
+    config.i18n.default_locale = :ba
     config.assets.paths << "#{config.root}/app/assets/fonts"
     config.assets.precompile += %w( .svg .eot .woff .ttf .otf )
 
